9023e0a1f1525f433ea2b5ca1e9b9d3714e1f9dd
Paulo Pereira <paulo.pereira@karhoo.com>
2020-07-21 16:11:08 +0100

    send to pulsar

--- poller/cmd/poller/main.go
+++ poller/cmd/poller/main.go
@@ -8,7 +8,9 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/apache/pulsar-client-go/pulsar"
 	"github.com/caarlos0/env/v6"
 	_ "github.com/lib/pq"
 	"github.com/quintans/eventstore/common"
@@ -16,11 +18,21 @@ import (
 )
 
 type Config struct {
+	ConfigDb
+	ConfigPulsar
+}
+
+type ConfigDb struct {
 	DbUser     string `env:"DB_USER" envDefault:"root"`
 	DbPassword string `env:"DB_PASSWORD" envDefault:"password"`
 	DbHost     string `env:"DB_HOST"`
 	DbPort     int    `env:"DB_PORT" envDefault:"5432"`
 	DbName     string `env:"DB_NAME" envDefault:"accounts"`
+}
+
+type ConfigPulsar struct {
+	PulsarAddress string `env:"PULSAR_ADDRESS"`
+	Topic         string `env:"TOPIC" envDefault:"accounts"`
 }
 
 func main() {
@@ -30,12 +42,19 @@ func main() {
 		log.Fatal(err)
 	}
 
+	p, err := NewPulsarSink(cfg.ConfigPulsar)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer p.Close()
+
 	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.DbPort, cfg.DbName)
 	tracker, err := poller.NewPgRepository(dbURL)
 	if err != nil {
 		log.Fatal(err)
 	}
-	lm := poller.New(tracker, poller.WithStartFrom(poller.BEGINNING))
+	lm := poller.New(tracker)
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
@@ -48,11 +67,96 @@ func main() {
 		cancel()
 	}()
 
-	lm.Handle(ctx, func(ctx context.Context, e common.Event) {
-		b, err := json.Marshal(e)
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println(string(b))
+	lm.Forward(ctx, p)
+}
+
+const (
+	EventIDKey       = "EventID"
+	AggregateTypeKey = "AggregateType"
+)
+
+type PulsarSink struct {
+	topic    string
+	client   pulsar.Client
+	producer pulsar.Producer
+}
+
+// NewPulsarSink instantiate PulsarSink
+func NewPulsarSink(cfg ConfigPulsar) (*PulsarSink, error) {
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL: "pulsar://" + cfg.PulsarAddress,
 	})
+	if err != nil {
+		return nil, fmt.Errorf("Could not instantiate Pulsar client: %w", err)
+	}
+
+	producer, err := client.CreateProducer(pulsar.ProducerOptions{
+		Topic: cfg.Topic,
+	})
+	if err != nil {
+		client.Close()
+		return nil, fmt.Errorf("Could not instantiate Pulsar producer: %w", err)
+	}
+
+	return &PulsarSink{
+		topic:    cfg.Topic,
+		client:   client,
+		producer: producer,
+	}, nil
+}
+
+// Close releases resources blocking until
+func (p *PulsarSink) Close() {
+	if p.producer != nil {
+		p.producer.Close()
+	}
+	if p.client != nil {
+		p.client.Close()
+	}
+}
+
+// LastEventID gets the last event sent to pulsar
+func (p *PulsarSink) LastEventID(ctx context.Context) (string, error) {
+	reader, err := p.client.CreateReader(pulsar.ReaderOptions{
+		Topic:                   p.topic,
+		StartMessageID:          pulsar.LatestMessageID(),
+		StartMessageIDInclusive: true,
+	})
+	if err != nil {
+		return "", fmt.Errorf("Unable to create reader for topic %s: %w", p.topic, err)
+	}
+	defer reader.Close()
+
+	if reader.HasNext() {
+		msg, err := reader.Next(ctx)
+		if err != nil {
+			return "", fmt.Errorf("Unable to read last message in topic %s: %w", p.topic, err)
+		}
+		eid := msg.Properties()[EventIDKey]
+		return eid, nil
+	}
+
+	return "", nil
+}
+
+// Send sends the event to pulsar
+func (p *PulsarSink) Send(ctx context.Context, e common.Event) {
+	b, err := json.Marshal(e)
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = p.producer.Send(context.Background(), &pulsar.ProducerMessage{
+		Payload: b,
+		Properties: map[string]string{
+			EventIDKey:       e.ID,
+			AggregateTypeKey: e.AggregateType,
+		},
+		Key:       e.AggregateID,
+		EventTime: time.Now(),
+	})
+	if err != nil {
+		fmt.Println("Failed to send message", err)
+	}
+
 }
--- poller/go.mod
+++ poller/go.mod
@@ -3,9 +3,10 @@ module github.com/quintans/es-cqrs-bank-transfer/poller
 go 1.14
 
 require (
+	github.com/apache/pulsar-client-go v0.1.1
 	github.com/caarlos0/env/v6 v6.3.0
 	github.com/lib/pq v1.3.0
-	github.com/quintans/eventstore v0.0.0-20200720205834-a4b40139f841
+	github.com/quintans/eventstore v0.0.0-20200721134350-7b18ecd69d0a
 )
 
 // replace github.com/quintans/eventstore => ../../eventstore
